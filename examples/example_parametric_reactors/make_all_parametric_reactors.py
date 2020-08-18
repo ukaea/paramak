c69540556b7ab6727e3e03f52eb68cecb56d27c7
billingsley-john <johnbillingsley1996@yahoo.com>
2020-08-18 13:41:07 +0100

    added single null reactor examples

--- examples/example_parametric_reactors/make_all_parametric_reactors.py
+++ examples/example_parametric_reactors/make_all_parametric_reactors.py
@@ -49,7 +49,7 @@ def main():
         pf_coil_vertical_thicknesses=[50, 50, 50, 50],
         pf_coil_to_rear_blanket_radial_gap=50,
         pf_coil_to_tf_coil_radial_gap=50,
-        tf_coil_radial_thickness=100,
+        outboard_tf_coil_radial_thickness=100,
         tf_coil_poloidal_thickness=50,
     )
     my_reactor.name = "BallReactor_with_pf_tf_coils"
@@ -101,6 +101,75 @@ def main():
     )
 
     my_reactor.name = "SubmersionTokamak_with_pf_tf_coils"
+    all_reactors.append(my_reactor)
+
+    my_reactor = paramak.SingleNullReactor(
+        inner_bore_radial_thickness=50,
+        inboard_tf_leg_radial_thickness=50,
+        center_column_shield_radial_thickness=50,
+        divertor_radial_thickness=100,
+        inner_plasma_gap_radial_thickness=50,
+        plasma_radial_thickness=200,
+        outer_plasma_gap_radial_thickness=50,
+        firstwall_radial_thickness=50,
+        blanket_radial_thickness=100,
+        blanket_rear_wall_radial_thickness=50,
+        elongation=2,
+        triangularity=0.55,
+        number_of_tf_coils=16,
+        rotation_angle=180,
+        divertor_position="upper"
+    )
+
+    my_reactor.name = "SingleNullReactor_upper_divertor"
+    all_reactors.append(my_reactor)
+
+    my_reactor = paramak.SingleNullReactor(
+        inner_bore_radial_thickness=50,
+        inboard_tf_leg_radial_thickness=50,
+        center_column_shield_radial_thickness=50,
+        divertor_radial_thickness=100,
+        inner_plasma_gap_radial_thickness=50,
+        plasma_radial_thickness=200,
+        outer_plasma_gap_radial_thickness=50,
+        firstwall_radial_thickness=50,
+        blanket_radial_thickness=100,
+        blanket_rear_wall_radial_thickness=50,
+        elongation=2,
+        triangularity=0.55,
+        number_of_tf_coils=16,
+        rotation_angle=180,
+        divertor_position="lower"
+    )
+
+    my_reactor.name = "SingleNullReactor_lower_divertor"
+    all_reactors.append(my_reactor)
+
+    my_reactor = paramak.SingleNullReactor(
+        inner_bore_radial_thickness=50,
+        inboard_tf_leg_radial_thickness=50,
+        center_column_shield_radial_thickness=50,
+        divertor_radial_thickness=100,
+        inner_plasma_gap_radial_thickness=50,
+        plasma_radial_thickness=200,
+        outer_plasma_gap_radial_thickness=50,
+        firstwall_radial_thickness=50,
+        blanket_radial_thickness=100,
+        blanket_rear_wall_radial_thickness=50,
+        elongation=2,
+        triangularity=0.55,
+        number_of_tf_coils=16,
+        rotation_angle=180,
+        divertor_position="lower",
+        outboard_tf_coil_radial_thickness=50,
+        pf_coil_to_rear_blanket_radial_gap=50,
+        tf_coil_poloidal_thickness=70,
+        pf_coil_vertical_thicknesses=[50, 50, 50, 50, 50],
+        pf_coil_radial_thicknesses=[40, 40, 40, 40, 40],
+        pf_coil_to_tf_coil_radial_gap=50,
+    )
+
+    my_reactor.name = "SingleNullReactor_with_pf_tf_coils"
     all_reactors.append(my_reactor)
 
     return all_reactors
