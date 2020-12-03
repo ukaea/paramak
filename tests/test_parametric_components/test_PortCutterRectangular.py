5006a9674b6a9d5d945a513e12b5f99ec4cc1f82
billingsley-john <johnbillingsley1996@yahoo.com>
2020-12-03 09:50:44 +0000

    added global component and default parameter test

--- tests/test_parametric_components/test_PortCutterRectangular.py
+++ tests/test_parametric_components/test_PortCutterRectangular.py
@@ -6,17 +6,26 @@ import paramak
 
 class TestPortCutterRectangular(unittest.TestCase):
 
-    def test_creation(self):
-        """Checks a PortCutterRectangular creation."""
-
-        test_component = paramak.PortCutterRectangular(
-            center_point = (0, 0),
-            distance=3,
-            height=0.2,
-            width=0.4,
-            fillet_radius=0.02,
-            extrusion_start_offset=1,
-            azimuth_placement_angle=[0, 45, 90, 180]
+    def setUp(self):
+        self.test_shape = paramak.PortCutterRectangular(
+            width=40, height=40, distance=300
         )
 
-        assert test_component.solid is not None
+    def test_default_parameters(self):
+        """Checks that the default parameters of a PortCutterRectangular are correct."""
+
+        assert self.center_point = (0, 0)
+        assert self.workplane = "ZY"
+        assert self.rotation_axis == "Z"
+        assert self.extrusion_start_offset == 1
+        assert self.stp_filename == "PortCutterRectangular.stp"
+        assert self.stl_filename == "PortCutterRectangular.stl"
+        assert self.name == "rectangular_port_cutter"
+        assert self.material_tag == "rectangular_port_cutter_mat"
+
+    def test_creation(self):
+        """Creates a rectangular port cutter using the PortCutterRectangular parametric
+        component and checks that a cadquery solid is created."""
+
+        assert self.test_shape.solid is not None
+        assert self.test_shape.volume > 1000
