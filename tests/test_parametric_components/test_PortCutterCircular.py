e76844b97c48c2e75f55512b27c29f9149c54ed9
billingsley-john <johnbillingsley1996@yahoo.com>
2020-12-03 09:32:50 +0000

    added global component for tests and added default parameters test

--- tests/test_parametric_components/test_PortCutterCircular.py
+++ tests/test_parametric_components/test_PortCutterCircular.py
@@ -6,15 +6,26 @@ import paramak
 
 class test_component(unittest.TestCase):
 
-    def test_creation(self):
-        """Checks a PortCutterCircular creation."""
-
-        test_component = paramak.PortCutterCircular(
-            center_point=(0, 0),
-            distance=3,
-            radius=0.1,
-            extrusion_start_offset=1,
-            azimuth_placement_angle=[0, 45, 90, 180]
+    def setUp(self):
+        self.test_shape = paramak.PortCutterCircular(
+            distance=300, radius=20
         )
 
-        assert test_component.solid is not None
+    def test_default_parameters(self):
+        """Checks that the default parameters of a PortCutterCircular are correct."""
+
+        assert self.center_point == (0, 0)
+        assert self.workplane == "ZY"
+        assert self.rotation_axis == "Z"
+        assert self.extrusion_start_offset == 1
+        assert self.stp_filename == "PortCutterCircular.stp"
+        assert self.stl_filename == "PortCutterCircular.stl"
+        assert self.name == "circular_port_cutter"
+        assert self.material_tag == "circular_port_cutter_mat"
+
+    def test_creation(self):
+        """Creates a circular port cutter using the PortCutterCircular parametric
+        component and checks that a cadquery solid is created."""
+
+        assert self.test_shape.solid is not None
+        assert self.test_shape.volume > 1000 
