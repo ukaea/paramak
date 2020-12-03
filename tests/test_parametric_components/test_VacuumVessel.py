aaa601f549e5d3f9b2a961dbd5c59f42a90a2424
billingsley-john <johnbillingsley1996@yahoo.com>
2020-12-03 14:56:12 +0000

    updated test to use new port cutters

--- tests/test_parametric_components/test_VacuumVessel.py
+++ tests/test_parametric_components/test_VacuumVessel.py
@@ -22,15 +22,16 @@ class TestVacuumVessel(unittest.TestCase):
         caquery solid is created."""
 
         cutter1 = paramak.PortCutterRectangular(
-            distance=3, z_pos=0, height=0.2, width=0.4, fillet_radius=0.01)
+            center_point=(0, 0),
+            distance=3, extrusion_start_offset=0, height=0.2, width=0.4, fillet_radius=0.01)
         cutter2 = paramak.PortCutterRectangular(
-            distance=3, z_pos=0.5, height=0.2, width=0.4, fillet_radius=0.00)
+            center_point=(0.5, 0),
+            distance=3, extrusion_start_offset=0, height=0.2, width=0.4, fillet_radius=0.00)
         cutter3 = paramak.PortCutterRectangular(
-            distance=3, z_pos=-0.5, height=0.2, width=0.4,
-            physical_groups=None)
+            distance=3, center_point=(-0.5, 0), height=0.2, width=0.4)
         cutter4 = paramak.PortCutterCircular(
-            distance=3, z_pos=0.25, radius=0.1, azimuth_placement_angle=45,
-            physical_groups=None)
+            center_point=(0.25, 0),
+            distance=3, extrusion_start_offset=0, radius=0.1, azimuth_placement_angle=45)
         cutter5 = paramak.PortCutterRotated(
             (0, 0), azimuth_placement_angle=-90, rotation_angle=10,
             fillet_radius=0.01, physical_groups=None)
