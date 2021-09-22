28887e7f7516a6ca9777db96e42715715f59448f
billingsley-john <johnbillingsley1996@yahoo.com>
2021-09-22 17:21:44 +0100

    updated example

--- examples/example_parametric_reactors/ball_reactor_layered.py
+++ examples/example_parametric_reactors/ball_reactor_layered.py
@@ -13,7 +13,7 @@ my_reactor = paramak.BallReactorLayered(
     elongation=2,
     triangularity=0.55,
     number_of_tf_coils=16,
-    rotation_angle=180,
+    rotation_angle=360,
     pf_coil_case_thicknesses=[10, 10, 10, 10],
     pf_coil_radial_thicknesses=[20, 50, 50, 20],
     pf_coil_vertical_thicknesses=[20, 50, 50, 20],
@@ -23,8 +23,6 @@ my_reactor = paramak.BallReactorLayered(
     outboard_tf_coil_radial_thickness=100,
     outboard_tf_coil_poloidal_thickness=50
 )
-
-my_reactor.export_stp('BallReactorLayered')
 
 my_source = openmc.Source()
 my_source.space = openmc.stats.Point((my_reactor.major_radius, 0, 0))
