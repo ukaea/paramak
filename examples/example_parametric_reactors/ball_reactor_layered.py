4cb0e98e5a528d8501cab6a6b38a24d844bd55a4
billingsley-john <johnbillingsley1996@yahoo.com>
2021-09-22 16:39:48 +0100

    added initial BallReactorLayered example python script

---
+++ examples/example_parametric_reactors/ball_reactor_layered.py
@@ -0,0 +1,26 @@
+
+import paramak
+
+my_reactor = paramak.BallReactorLayered(
+    inner_bore_radial_thickness=10,
+    inboard_layer_thicknesses=[20, 30, 50],
+    divertor_radial_thickness=150,
+    inner_plasma_gap_radial_thickness=30,
+    plasma_radial_thickness=300,
+    outer_plasma_gap_radial_thickness=30,
+    outboard_layer_thicknesses=[30, 40, 20, 20],
+    elongation=2,
+    triangularity=0.55,
+    number_of_tf_coils=16,
+    rotation_angle=180,
+    pf_coil_case_thicknesses=[10, 10, 10, 10],
+    pf_coil_radial_thicknesses=[20, 50, 50, 20],
+    pf_coil_vertical_thicknesses=[20, 50, 50, 20],
+    pf_coil_radial_position=[500, 575, 575, 500],
+    pf_coil_vertical_position=[300, 100, -100, -300],
+    rear_blanket_to_tf_gap=50,
+    outboard_tf_coil_radial_thickness=100,
+    outboard_tf_coil_poloidal_thickness=50
+)
+
+my_reactor.export_stp('BallReactorLayered')
